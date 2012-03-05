3bddf234bfd2e0d791cc51a8de45071ed95f4e24
Phillip Pollard <bennie@cpan.org>
2012-03-05 06:37:53 +0000

    Now with my usual build script and auto taging and versioning.

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+MODULE='lib/Term/Activity.pm'
+VERSION=`cvs status $MODULE | grep Working | awk '{ print $3 }' | tr '.' ' ' | awk '{ printf "v%d.%02d", $1, $2 }'`
+DATE=`date '+%Y/%m/%d'`
+YEAR=`date '+%Y'`
+TARDIR="Cache-Bounded-$VERSION";
+
+echo
+echo "Module  : $MODULE"
+echo "Version : $VERSION"
+echo "Date    : $DATE"
+echo "Year    : $YEAR"
+echo "Tar Dir : $TARDIR"
+echo
+
+if [ -d build ];
+  then echo "Cleaning Build directory:"; rm -rfv build; echo; 
+fi
+
+echo "Creating the build directory:"
+mkdir -pv "build/$TARDIR"
+echo
+
+echo "Copying files"
+rsync -av --files-from=MANIFEST ./ "build/$TARDIR/"
+echo
+
+echo "Updating date tags."
+find build -type f | xargs perl -p -i -e "s|DATETAG|$DATE|g" 
+echo
+
+echo "Updating version tags."
+find build -type f | xargs perl -p -i -e "s|VERSIONTAG|$VERSION|g" 
+echo
+
+echo "Updating date tags."
+find build -type f | xargs perl -p -i -e "s|YEARTAG|$YEAR|g" 
+echo
+
+echo "Building the tar file."
+cd build && tar czvf $TARDIR.tar.gz $TARDIR
+echo
+
+echo DONE!
