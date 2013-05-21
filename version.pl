33312e3c12f5dcbc4ef2e1b40c37b7ae813be19f
bennie@cpan.org <bennie@cpan.org>
2013-05-20 23:26:07 -0700

    Updating to include github support for version bumping.

--- version.pl
+++ version.pl
@@ -10,7 +10,8 @@ if ( $ARGV[0] eq '--bump' ) {
   my $next_version = &next($version);
   print "Bumping version from $version to $next_version\n";
   system("git tag -a $next_version -m 'Version $next_version'");
-  system("git push --tags");
+  system("git push --tags origin master");
+  system("git push --tags github master");
 } elsif ( $ARGV[0] eq '--build' ) {
   print &build();
 } else {
